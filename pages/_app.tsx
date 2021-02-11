5310284c8adbcdc4e065d02b89cfd06880e81d4c
Louis Smit <louissmit97@gmail.com>
2021-02-11 19:55:35 +0200

    Removed theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
-import theme from '../theme'
 import { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider resetCSS>
       <Component {...pageProps} />
     </ChakraProvider>
   )
