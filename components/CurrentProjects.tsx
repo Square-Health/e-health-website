9a236ba5dcd5d1b462c0f099f849858e301e7304
Louis Smit <louissm.it@icloud.com>
2022-02-07 15:21:00 +0200

    Temp changes

--- components/CurrentProjects.tsx
+++ components/CurrentProjects.tsx
@@ -16,9 +16,9 @@ import { CheckCircleIcon } from '@chakra-ui/icons'
 const HerohCard = ({ title, children }) => (
   <InfoCard title={title} children={children} productName="Heroh" />
 )
-const SquareCard = ({ title, children }) => (
-  <InfoCard title={title} children={children} productName="Square" />
-)
+// const SquareCard = ({ title, children }) => (
+//   <InfoCard title={title} children={children} productName="Square" />
+// )
 
 const CurrentProjects = () => {
   return (
@@ -216,117 +216,117 @@ const Product = ({ href, title, description, img, children }) => (
   </Stack>
 )
 
-const SH = () => {
-  return (
-    <Product
-      href="https://square.health"
-      img="/icon.png"
-      title="Square Health"
-      description="A fresh approach to collaborative healthcare, starting with the individual."
-    >
-      <Stack justify="flex-start" spacing={6}>
-        <SquareCard title="eHealth">
-          Covid-19 emphasised the challenges brought forward by a fragmented
-          healthcare industry. The bottom line is individuals’ health data is
-          spread amongst a myriad of silos - even paper. In addition, there
-          exists no interoperability between these solutions, and while each
-          individual or patient has a right to their health data, it is simply
-          not possible in today’s industry. But it can be.
-        </SquareCard>
+// const SH = () => {
+//   return (
+//     <Product
+//       href="https://square.health"
+//       img="/icon.png"
+//       title="Square Health"
+//       description="A fresh approach to collaborative healthcare, starting with the individual."
+//     >
+//       <Stack justify="flex-start" spacing={6}>
+//         <SquareCard title="eHealth">
+//           Covid-19 emphasised the challenges brought forward by a fragmented
+//           healthcare industry. The bottom line is individuals’ health data is
+//           spread amongst a myriad of silos - even paper. In addition, there
+//           exists no interoperability between these solutions, and while each
+//           individual or patient has a right to their health data, it is simply
+//           not possible in today’s industry. But it can be.
+//         </SquareCard>
 
-        <SquareCard title="Participative healthcare">
-          Participative healthcare defines the shift from a paternalistic
-          industry towards a more inclusive and participative approach which
-          includes shared decision making. This participative approach does not
-          only involve the healthcare provider or doctor or a multiple thereof,
-          but also the patient. However, where applicable, a designated proxy
-          should be empowered with capability to take responsibility for
-          community based care on behalf of its members.
-        </SquareCard>
+//         <SquareCard title="Participative healthcare">
+//           Participative healthcare defines the shift from a paternalistic
+//           industry towards a more inclusive and participative approach which
+//           includes shared decision making. This participative approach does not
+//           only involve the healthcare provider or doctor or a multiple thereof,
+//           but also the patient. However, where applicable, a designated proxy
+//           should be empowered with capability to take responsibility for
+//           community based care on behalf of its members.
+//         </SquareCard>
 
-        <SquareCard title="Shifting the doctors’ focus">
-          Participative healthcare empowers the individual to take
-          co-responsibility for triage. This in effect reduces the load on the
-          healthcare industry and allows the doctors or healthcare workers to
-          only focus on the cases requiring definitive prognosis or care.
-          Finally, participative healthcare shifts the doctor’s focus from
-          reactively treating ill patients to proactively managing the health of
-          individuals.
-        </SquareCard>
+//         <SquareCard title="Shifting the doctors’ focus">
+//           Participative healthcare empowers the individual to take
+//           co-responsibility for triage. This in effect reduces the load on the
+//           healthcare industry and allows the doctors or healthcare workers to
+//           only focus on the cases requiring definitive prognosis or care.
+//           Finally, participative healthcare shifts the doctor’s focus from
+//           reactively treating ill patients to proactively managing the health of
+//           individuals.
+//         </SquareCard>
 
-        {/* <InfoCard title="Proactive health management">
-          A diabetic performing daily self-assessments, with the
-          collective data over time identifying a trend which triggers a flag
-          with the doctor associated the health record? This would thus
-          result in the doctor proactively managing the case, example calling
-          the patient discuss concerns or requesting the patient visit the
-          practice for a physical examination. The doctor is now proactively
-          manage the patient’s health condition instead of reactively treating a
-          sick person after the fact.
-        </InfoCard> */}
+//         {/* <InfoCard title="Proactive health management">
+//           A diabetic performing daily self-assessments, with the
+//           collective data over time identifying a trend which triggers a flag
+//           with the doctor associated the health record? This would thus
+//           result in the doctor proactively managing the case, example calling
+//           the patient discuss concerns or requesting the patient visit the
+//           practice for a physical examination. The doctor is now proactively
+//           manage the patient’s health condition instead of reactively treating a
+//           sick person after the fact.
+//         </InfoCard> */}
 
-        <SquareCard title="Extending reach to vulnerable communities">
-          A designated community carer performing participative care
-          peer-assessments should be alerted to issue TB or other medication,
-          while being reminded what peer-assessments to perform, for whom, when
-          and in the case of medication, what to administer.
-        </SquareCard>
-      </Stack>
-      {/* <Divider /> */}
-      <Stack pt={10}>
-        <Text
-          fontSize="sm"
-          textAlign="center"
-          textTransform="uppercase"
-          letterSpacing="wide"
-          fontWeight="medium"
-          color="gray.700"
-        >
-          Mission
-        </Text>
+//         <SquareCard title="Extending reach to vulnerable communities">
+//           A designated community carer performing participative care
+//           peer-assessments should be alerted to issue TB or other medication,
+//           while being reminded what peer-assessments to perform, for whom, when
+//           and in the case of medication, what to administer.
+//         </SquareCard>
+//       </Stack>
+//       {/* <Divider /> */}
+//       <Stack pt={10}>
+//         <Text
+//           fontSize="sm"
+//           textAlign="center"
+//           textTransform="uppercase"
+//           letterSpacing="wide"
+//           fontWeight="medium"
+//           color="gray.700"
+//         >
+//           Mission
+//         </Text>
 
-        <List mt={4} spacing={2} fontSize="sm">
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Address fragmentation in the healthcare industry
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Broaden triage through participative healthcare
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Shift medical professionals’ focus to proactive management instead
-            of reactive treatment
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Reduce workload in the healthcare industry
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Extend healthcare reach to vulnerable communities
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Enable interoperability amongst solution providers
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Take the 1st steps to a true digital health passport
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Extend capability to address Occupational Health in the informal
-            (mining-) sector
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="orange.400" />
-            Last, but not least, to extend our vehicle to address wrongs of the
-            past (retired mineworkers; refer silicosis & TB)
-          </ListItem>
-        </List>
-      </Stack>
-    </Product>
-  )
-}
+//         <List mt={4} spacing={2} fontSize="sm">
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Address fragmentation in the healthcare industry
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Broaden triage through participative healthcare
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Shift medical professionals’ focus to proactive management instead
+//             of reactive treatment
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Reduce workload in the healthcare industry
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Extend healthcare reach to vulnerable communities
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Enable interoperability amongst solution providers
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Take the 1st steps to a true digital health passport
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Extend capability to address Occupational Health in the informal
+//             (mining-) sector
+//           </ListItem>
+//           <ListItem>
+//             <ListIcon as={CheckCircleIcon} color="orange.400" />
+//             Last, but not least, to extend our vehicle to address wrongs of the
+//             past (retired mineworkers; refer silicosis & TB)
+//           </ListItem>
+//         </List>
+//       </Stack>
+//     </Product>
+//   )
+// }
